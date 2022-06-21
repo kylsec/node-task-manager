2352428167be25724e32620a66837984419bc6b5
kfletch87 <kfletch1987@gmail.com>
2022-06-21 16:40:50 +0100

    main

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,31 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
 const { createCustomError } = require('../errors/custom-error')
+
+// Get all tasks
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
 })
 
-const createTask = asyncWrapper(async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({ task })
-})
-
+// Get single task
 const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
   const task = await Task.findOne({ _id: taskID })
   if (!task) {
     return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-
   res.status(200).json({ task })
 })
+
+// Create task
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
+
+
+// Delete task
 const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
   const task = await Task.findOneAndDelete({ _id: taskID })
@@ -28,18 +34,17 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
   }
   res.status(200).json({ task })
 })
+
+// Update task
 const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
-
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true,
   })
-
   if (!task) {
     return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-
   res.status(200).json({ task })
 })
 
--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'must provide name'],
+    required: [true, 'You must provide a task name.'],
     trim: true,
-    maxlength: [20, 'name can not be more than 20 characters'],
+    maxlength: [30, 'Task name must not be more than 30 characters.'],
   },
   completed: {
     type: Boolean,
